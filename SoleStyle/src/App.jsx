2d2f2dc0f1d58b913c82595bc7ae3e22454656c0
kdonrmadusanka <ravmkdon@gmail.com>
2025-03-31 23:44:54 +0530

    Update product.

--- SoleStyle/src/App.jsx
+++ SoleStyle/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Nav from './components/navigation/nav'
+import Product from './components/product/product';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Nav/>
+      <Nav />
+      <Product />
     </>
   )
 }
