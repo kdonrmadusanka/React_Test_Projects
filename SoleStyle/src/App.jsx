4cfaf8ca3aded9dade2474e4d53a77b34514a703
kdonrmadusanka <ravmkdon@gmail.com>
2025-04-02 14:45:18 +0530

    Update App.jsx.

--- SoleStyle/src/App.jsx
+++ SoleStyle/src/App.jsx
@@ -1,14 +1,24 @@
 import { useState } from 'react'
 import Nav from './components/navigation/nav'
 import Product from './components/product/product';
+import Recomended from './components/recomended/recomended';
+import SideBar from './sideBar/sideBar';
+import './app.css';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Nav />
-      <Product />
+    <div className='layout-container'>
+      <SideBar className="sidebar" />
+      <div className="main-content">
+        <Nav />
+        <Recomended />
+        <Product />
+      </div>
+    </div>
     </>
   )
 }
